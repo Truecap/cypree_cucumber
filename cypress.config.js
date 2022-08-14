0128a9b7fb411a880d8a37b225bfaf149758e0ea
Sl Sn <snigurslavik@gmail.com>
2022-08-14 14:34:08 +0300

    add new configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/my-test-output.xml',
+    toConsole: true
+  },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
