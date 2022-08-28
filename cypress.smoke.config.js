83313270234dea540e3c02c96744d84bff0f50ca
Sl Sn <snigurslavik@gmail.com>
2022-08-28 17:09:54 +0300

    back config

--- cypress.smoke.config.js
+++ cypress.smoke.config.js
@@ -8,7 +8,6 @@ module.exports = defineConfig({
   e2e: {
     
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cypress-cucumber-preprocessor);
       AllureWriter(on, config);
       //return config;
       return require('./cypress/plugins/index.js')(on, config)
