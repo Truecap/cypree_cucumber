d56999c593509917439f20677ba29003cdd709bf
Sl Sn <snigurslavik@gmail.com>
2022-08-30 12:02:09 +0300

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -6,14 +6,15 @@ module.exports = defineConfig({
     allureResultsPath: 'allure-results',
   },
   e2e: {
-  },
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       AllureWriter(on, config);
       return require('./cypress/plugins/index.js')(on, config)
     },
     excludeSpecPattern: '*.js',
     specPattern: 'cypress/e2e/Features/*.{feature,features}',
+    
+  },
+    
+    
   },
 )
